d360b64e8ba985002402582fbcae598e6a8872f6
sudheer2144 <116650566+sudheer2144@users.noreply.github.com>
2023-06-20 16:44:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+function getSize(){
+	
+	let x=document.documentElement.clientWidth;
+	let y=document.documentElement.clientHeight;
+
+	document.getElementById("sizeInfo").innerHTML="<h1>Width: "+x+" and Height: "+y+"</h1>";
+}
